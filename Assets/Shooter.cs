c86a705291a6584c0cd19b7d8a0bee5cdf54afdd
steven-xuk <xuk@scotscollege.school.nz>
2025-08-29 20:18:44 +1200

    pushing

--- Assets/Shooter.cs
+++ Assets/Shooter.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     public Transform firePoint;
-
+    public bool canShoot = true;
+    public ParticleSystem muzzleFlash;
     public GameObject hitEffect;
+
+    [SerializeField] float explosionForce = 10;
+    [SerializeField] float explosionRadius = 10;
+    Collider[] colliders = new Collider[100];
 
     // Update is called once per frame
     void Update()
@@ -17,7 +22,7 @@ public class Shooter : MonoBehaviour
                            (triggerAxis != null && triggerAxis.ReadValue() >= 0.5f);
 
         // Shooting
-        if (triggerHeld)
+        if (triggerHeld && canShoot)
         {
             Shoot();
         }
@@ -26,12 +31,37 @@ public class Shooter : MonoBehaviour
     public void Shoot()
     {
         RaycastHit hitInfo;
+        canShoot = false;
+        muzzleFlash.Play(true);
         bool hit = Physics.Raycast(firePoint.position, firePoint.forward, out hitInfo);
         if (hit)
         {
             Debug.Log(hitInfo.collider.gameObject.name);
+            ExplodeNonAlloc(hitInfo.point);
             Instantiate(hitEffect, hitInfo.point, hitEffect.transform.rotation);
             Destroy(hitEffect, 5);
         }
+        Invoke("EnableShooting", 0.5f);
+    }
+
+    void ExplodeNonAlloc(Vector3 position)
+    {
+        int numColliders = Physics.OverlapSphereNonAlloc(position, explosionRadius, colliders);
+
+        if (numColliders > 0)
+        {
+            for (int i = 0; i < numColliders; i++)
+            {
+                if (colliders[i].TryGetComponent(out Rigidbody rb))
+                {
+                    rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+                }
+            }
+        }
+    }
+
+    void EnableShooting()
+    {
+        canShoot = true;
     }
 }
